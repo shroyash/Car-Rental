a6f5c09e43202d259c379a02e24214c112746058
shroyash <shroyash35@gmail.com>
2024-10-24 07:37:05 +0545

    home page completed

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import HeroSlider from '../Component/UI/Heroslider'
-import About from '../Component/UI/About'
+import About from '../Component/UI/AboutSection'
 import Service from '../Component/UI/Service'
 import HotOffer from '../Component/UI/HotOffer'
 import Testimonials from '../Component/UI/Testimonials'
+import Blog from '../Component/UI/Blog'
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <Service/>
       <HotOffer/>
       <Testimonials/>
+      <Blog/>
     </section>
     </div>
   )
